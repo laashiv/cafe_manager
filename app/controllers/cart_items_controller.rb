89607b6191071c7afa2affdc0b762ab6b6f92290
laashiv <vishnav2502@gmail.com>
2020-06-10 16:37:24 +0530

    Rendering cdash

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -17,11 +17,11 @@ class CartItemsController < ApplicationController
     id = params[:id]
     sign = params[:sign]
     item = current_user.cart.cart_items.find(id)
-    if sign == 0
+    if sign == "0"
       item.quantity = item.quantity - 1
       item.save!
       cart.decrement_total(item.menu_item_price, 1, current_user)
-    else
+    elsif sign == "1"
       item.quantity = item.quantity + 1
       item.save!
       cart.increment_total(item.menu_item_price, current_user)
