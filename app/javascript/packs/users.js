e03be311320ec7a3b8c1b0a40a250c94717c55a7
laashiv <vishnav2502@gmail.com>
2020-06-24 15:25:15 +0530

    CSS optimizations

--- app/javascript/packs/users.js
+++ app/javascript/packs/users.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-  $(document).on("click", "#hide", function () {
+  $("#hide").on("click", function () {
+    $(this).toggleClass("fa-eye-slash fa-eye");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+  $("#re-hide").on("click", function () {
     $(this).toggleClass("fa-eye-slash fa-eye");
     var input = $($(this).attr("toggle"));
     if (input.attr("type") == "password") {
